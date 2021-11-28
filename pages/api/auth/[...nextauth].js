b66a856335fd6c4069028748ad89913b3722b3cf
mariosknl <mariosknl@outlook.com>
2021-11-28 07:43:09 +0200

    installed next auth & set the env variables

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+});
