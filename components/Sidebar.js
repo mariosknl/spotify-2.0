adc20870ec5a8b77b4d56481922f62700171151d
mariosknl <mariosknl@outlook.com>
2021-11-29 10:05:52 +0200

    customizing the login page

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,11 +6,18 @@ import {
   HeartIcon,
   RssIcon,
 } from "@heroicons/react/outline";
+import { signOut } from "next-auth/react";
 
 export default function Sidebar() {
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Log Out</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,31 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { LOGIN_URL } from "../../../lib/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log("REFRESHED TOKEN IS", refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      // Replace if new one came back else fall back to the old refresh token
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -25,8 +50,27 @@ export default NextAuth({
           accessToken: account.access_token,
           refreshToken: account.refresh_token,
           username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
         };
       }
+
+      // return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log("Existing Access Token is valid");
+        return token;
+      }
+
+      // access token has expired, so we need to refresh it...
+      console.log("Access token has expired, refreshing");
+      return await refreshAccessToken(token);
+    },
+
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
     },
   },
 });
